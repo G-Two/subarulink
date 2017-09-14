e777840345e74f32788a6d3021b8e637c59299db
Sergey Isachenko <vortexius@gmail.com>
2017-09-14 16:21:36 +0300

    Fix for issues like: "tesla lock took longer than the scheduled update interval 0:00:30" in HASS.

--- teslajsonpy/controller.py
+++ teslajsonpy/controller.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 from threading import RLock
 
 from teslajsonpy.connection import Connection
@@ -6,6 +7,7 @@ from teslajsonpy.BatterySensor import Battery
 from teslajsonpy.Lock import Lock
 from teslajsonpy.Climate import Climate, TempSensor
 from teslajsonpy.BinarySensor import ParkingSensor, ChargerConnectionSensor
+from teslajsonpy.Charger import ChargerSwitch
 from teslajsonpy.GPS import GPS
 
 
@@ -22,15 +24,16 @@ class Controller:
         self.__lock = RLock()
 
         cars = self.__connection.get('vehicles')['response']
+
         for car in cars:
             self.__last_update_time[car['id']] = 0
-            self.wake_up(car['id'])
             self.update(car['id'])
             self.__vehicles.append(Climate(car, self))
             self.__vehicles.append(Battery(car, self))
             self.__vehicles.append(TempSensor(car, self))
             self.__vehicles.append(Lock(car, self))
             self.__vehicles.append(ChargerConnectionSensor(car, self))
+            self.__vehicles.append(ChargerSwitch(car, self))
             self.__vehicles.append(ParkingSensor(car, self))
             self.__vehicles.append(GPS(car, self))
 
@@ -56,10 +59,11 @@ class Controller:
         if time.time() - self.__last_update_time[car_id] > self.update_interval:
             self.__lock.acquire()
             self.wake_up(car_id)
-            self.__climate[car_id] = self.data_request(car_id, 'climate_state')
-            self.__charging[car_id] = self.data_request(car_id, 'charge_state')
-            self.__state[car_id] = self.data_request(car_id, 'vehicle_state')
-            self.__driving[car_id] = self.data_request(car_id, 'drive_state')
+            data = self.get(car_id, 'data')['response']
+            self.__climate[car_id] = data['climate_state']
+            self.__charging[car_id] = data['charge_state']
+            self.__state[car_id] = data['vehicle_state']
+            self.__driving[car_id] = data['drive_state']
             self.__last_update_time[car_id] = time.time()
             self.__lock.release()
 

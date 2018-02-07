7d22342f611971d786bfa640cb21a0c252894b99
Sergey Isachenko <vortexius@gmail.com>
2018-02-07 14:40:35 +0300

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='teslajsonpy',
-    version='0.0.20',
+    version='0.0.22',
     packages=['teslajsonpy'],
     include_package_data=True,
     python_requires='>=3',

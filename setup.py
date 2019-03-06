d15535865dc1a46519d43cbd22fe46bf4cdbc296
Alan Tse <alandtse@users.noreply.github.com>
2019-03-05 20:35:06 -0800

    Bump version to 0.0.25 (#40)
    * Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='teslajsonpy',
-    version='0.0.24',
+    version='0.0.25',
     packages=['teslajsonpy'],
     include_package_data=True,
     python_requires='>=3',

ab004739c5f28390c265a060c26c69dc86fdac5f
G-Two <g_two_github@fastmail.com>
2020-07-23 19:47:26 -0400

    Fixes #11 - Remote Start working for Ascent

--- subarulink/__version__.py
+++ subarulink/__version__.py
@@ -5,4 +5,4 @@ subarulink - A Python Package for interacting with Subaru Starlink Remote Servic
 For more details about this api, please refer to the documentation at
 https://github.com/G-Two/subarulink
 """
-__version__ = "0.3.2"
+__version__ = "0.3.3"

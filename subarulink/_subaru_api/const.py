badcb1710984398fb180f4e520c18e240dbb28f4
G-Two <7310260+G-Two@users.noreply.github.com>
2023-02-11 13:47:49 -0500

    Update API to g2v25

--- subarulink/_subaru_api/const.py
+++ subarulink/_subaru_api/const.py
@@ -2,10 +2,10 @@
 """
 Constants used by subarulink to interact with the STARLINK HTTP API.
 
-This is an undocumented API derived from analysis of the MySubaru Android app v2.7.30.
+This is an undocumented API derived from analysis of the MySubaru Android app v2.7.39.
 """
 
-API_VERSION = "/g2v24"
+API_VERSION = "/g2v25"
 
 API_SERVER = {
     "USA": "mobileapi.prod.subarucs.com",
